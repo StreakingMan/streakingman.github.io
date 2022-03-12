b83f8be1736e602536d9d5116ce788e78fed853f
streakingman <z_max_y@163.com>
2022-03-13 02:42:56 +0800

    test: ...

--- _plugins/del-all-fk-json-invalid.rb
+++ _plugins/del-all-fk-json-invalid.rb
@@ -3,13 +3,13 @@ module Jekyll
     def remove_fk_chars(input)
       input.gsub! '\"',''
       input.gsub! '\\',''
-      input.gsub! '\/',''
-      input.gsub! '\b',''
-      input.gsub! '\f',''
-      input.gsub! '\n',''
-      input.gsub! '\r',''
-      input.gsub! '\t',''
-      input.gsub! '\u',''
+      input.gsub! '\\/',''
+      input.gsub! '\\b',''
+      input.gsub! '\\f',''
+      input.gsub! '\\n',''
+      input.gsub! '\\r',''
+      input.gsub! '\\t',''
+      input.gsub! '\\u',''
     end
   end
 end
